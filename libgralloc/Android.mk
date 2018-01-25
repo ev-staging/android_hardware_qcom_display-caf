2e4f7cf59a531950f4f4d2cf5e6cff669900c990
Steven Moreland <smoreland@google.com>
2018-01-25 14:10:55 -0800

    display: build with VNDK
    Import headers for enabling VNDK.
    
    These modules don't explicitly declare their
    dependence on hardware/gralloc1.h
    
    Bug: 69526027
    Test: mma -j BOARD_VNDK_VERSION=current
    Change-Id: If9e5636635c7d039d7fa924cb51fa5f838ee6bca
    
    Make light build with the VNDK.
    
    When the vndk is enabled (BOARD_VNDK_VERSION=current),
    vendor libraries must only link against vendor variants
    in the build system. This also means that they receive
    restricted sets of headers and that automatic global
    headers are restricted.
    
    Test: build light with BOARD_VNDK_VERSION=current
    Bug: 69526027
    Change-Id: I8ad1da8aa2802a93493c877644266995c59f18ab
    
    Make libqservice build with the VNDK.
    
    When the vndk is enabled (BOARD_VNDK_VERSION=current),
    vendor libraries must only link against vendor variants
    in the build system. This also means that they receive
    restricted sets of headers and that automatic global
    headers are restricted.
    
    Test: build libqservice with BOARD_VNDK_VERSION=current
    Bug: 69526027
    Change-Id: Id98bf5e2ecc6f514f1d4dd9580d25027f36e0d86

--- libgralloc/Android.mk
+++ libgralloc/Android.mk
@@ -28,6 +28,7 @@ LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_HEADER_LIBRARIES        := libhardware_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc
 LOCAL_SHARED_LIBRARIES        += libqdutils libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\"
@@ -41,6 +42,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libmemalloc
 LOCAL_MODULE_TAGS             := optional
+LOCAL_HEADER_LIBRARIES        := libhardware_headers
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdutils libdl
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdmemalloc\"
--- libqdutils/Android.mk
+++ libqdutils/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE                  := libqdutils
 LOCAL_MODULE_TAGS             := optional
+LOCAL_HEADER_LIBRARIES        := libhardware_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libbinder libqservice libui
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdutils\"
@@ -19,6 +20,7 @@ include $(CLEAR_VARS)
 
 LOCAL_EXPORT_C_INCLUDE_DIRS   := $(LOCAL_PATH)
 LOCAL_VENDOR_MODULE             := true
+LOCAL_HEADER_LIBRARIES          := libhardware_headers
 LOCAL_SHARED_LIBRARIES          := liblog libcutils
 LOCAL_C_INCLUDES                := $(common_includes)
 LOCAL_ADDITIONAL_DEPENDENCIES   := $(common_deps)
